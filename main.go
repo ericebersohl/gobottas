1832180cdd95780d3d0cbdbff672bbc8222719f4
Eric <eric.ebersohl.1@gmail.com>
2019-06-01 20:40:42 -0500

    Set up initial structure

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"os"
 
-	dgo "github.com/bwmarrin/discordgo"
-	gde "github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -14,49 +14,26 @@ var (
 )
 
 func main() {
-	err := gde.Load()
-	errCheck("Failed to load from .env", err)
+	// Load Environment Variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load variables from .env.\n%v", err)
+	}
 
-	discord, err := dgo.New("Bot " + os.Getenv("AUTH"))
-	errCheck("Failed to create a discord client.", err)
+	commandPrefix = "`"
 
-	user, err := discord.User("@me")
-	errCheck("Failed to retrieve user account", err)
+	// Get Connection to Server
+	discord, err := discordgo.New("Bot " + os.Getenv("AUTH"))
+	if err != nil {
+		log.Fatalf("Failed to create discord client.\n%v", err)
+	}
 
-	botID = user.ID
-	discord.AddHandler(commandHandler)
-	discord.AddHandler(func(discord *dgo.Session, ready *dgo.Ready) {
-		err = discord.UpdateStatus(0, "Gobottas initialized.")
-		if err != nil {
-			log.Println("Failed to set status.")
-		}
-
-		servers := discord.State.Guilds
-		log.Printf("Gobottas is running on %d servers\n", len(servers))
-	})
-
-	err = discord.Open()
-	errCheck("Failed to connect to discord.", err)
+	// Open the connection
+	if err := discord.Open(); err != nil {
+		log.Fatalf("Failed to open connection.\n%v", err)
+	}
 	defer discord.Close()
 
-	commandPrefix = "&"
-
-	<-make(chan struct{})
-}
-
-func errCheck(msg string, err error) {
-	if err != nil {
-		log.Printf("%s: %+v", msg, err)
-		panic(err)
-	}
-}
-
-func commandHandler(discord *dgo.Session, message *dgo.MessageCreate) {
-	user := message.Author
-	if user.ID == botID || user.Bot {
-		return // bot is talking, does not react to itself
-	}
-
-	// content := message.Content
-	log.Printf("Message: %+v || From %s\n", message.Message, message.Author)
+	// keep main open indefinitely
+	<-make(chan interface{})
 }
